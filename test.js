82527f62de1c111301d31f68a85203c24371ce09
Vladimir Starkov <iamstarkov@gmail.com>
2015-11-08 10:55:06 +0100

    add tests

--- test.js
+++ test.js
@@ -1,8 +1,11 @@
-import { equal } from 'assert';
+import test from 'tape';
 import npmProfileDownloads from './index';
 
-it('should npmProfileDownloads', () =>
-  equal(npmProfileDownloads('unicorns'), 'unicorns'));
-
-it('should npmProfileDownloads invalid input', () =>
-  equal(npmProfileDownloads(), undefined));
+test('npmProfileDownloads', { timeout: 30000 }, ({ equal, assert, end } = t) => {
+  npmProfileDownloads('last-month', 'iamstarkov', (err, res) => {
+    if (err) throw err;
+    equal(typeof res, 'number', 'should be number');
+    assert(res > 10000, 'should greater 10000');
+    end();
+  });
+});

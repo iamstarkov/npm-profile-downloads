de00e3b8917f09a336e3416e1de1cfff41b82cdd
Vladimir Starkov <iamstarkov@gmail.com>
2015-11-08 11:18:11 +0100

    remove console.log

--- test.js
+++ test.js
@@ -4,7 +4,6 @@ import npmProfileDownloads from './index';
 test('npmProfileDownloads', { timeout: 30000 }, ({ equal, assert, end } = t) => {
   npmProfileDownloads('last-month', 'sindresorhus', (err, res) => {
     if (err) throw err;
-    console.log(res);
     equal(typeof res, 'number', 'should be number');
     assert(res > 300000000, 'should greater 300000000');
     end();

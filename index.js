a3c16cb050e789cbab576a33b1ca42c71c2bef88
andrepolischuk <andre.polischuk@gmail.com>
2016-02-11 18:34:16 +0300

    promisify

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import rest from 'rest-facade';
 import Registry from 'npm-stats';
 import { map, pipe, prop, filter, is, sum,
-  partial, join, splitEvery, toPairs, tail, flatten } from 'ramda';
+  join, splitEvery, toPairs, tail, flatten } from 'ramda';
 
 const packages = username => new Promise((resolve, reject) => {
   Registry().user(username).list((err, res) => {
@@ -14,14 +14,12 @@ const downloads = (period, pkg) =>
   new rest.Client(`${api}/downloads/point/:period/:package`)
     .get({ period, package: pkg });
 
-export default function npmProfileDownloads(period, username, cb) {
-  packages(username)
+export default function npmProfileDownloads(period, username) {
+  return packages(username)
     .then(pipe(splitEvery(200), map(join(','))))
     .then(map(bulk => downloads(period, bulk)))
     .then(bulk => Promise.all(bulk))
     .then(pipe(map(pipe(toPairs, map(tail), flatten)), flatten))
     .then(pipe(map(prop('downloads')), filter(is(Number))))
-    .then(sum)
-    .then(partial(cb, [null]))
-    .catch(cb);
+    .then(sum);
 };
